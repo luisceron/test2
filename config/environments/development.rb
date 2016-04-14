d7ac6096118d46456db1b4dab8a00883efd01fa1
Luis Ceron <luismarcoceron@gmail.com>
2016-04-14 20:46:39 +0000

    Fix message after logout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,11 @@ Rails.application.configure do
 
   # Added for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # For send emails to mailcatcher in development environment
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
 end
