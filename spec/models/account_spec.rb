5247bfbe0ee22796d44ad4c2920a931ec9ed6ca8
Luis Ceron <luismarcoceron@gmail.com>
2016-06-28 17:02:47 +0000

    Small adjusts in Accounts Tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   context "must validate" do
-    it "must validate required attributes" do
+    it "required attributes" do
       expect(subject).to validate_presence_of(:account_type)
       expect(subject).to validate_presence_of(:name)
     end
 
-    it "must validate unique attributes" do
+    it "unique attributes" do
       expect(subject).to validate_uniqueness_of(:name).case_insensitive.scoped_to(:user_id)
     end
   end
