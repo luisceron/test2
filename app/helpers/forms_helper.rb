dae4d061e8338dc315a87418ee0525c0ea9f2bef
Luis Ceron <luismarcoceron@gmail.com>
2016-07-21 19:21:50 +0000

    Do not allow edit account balance

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,5 +1,5 @@
 module FormsHelper
-  def not_editable_for(object, attribute, form)
-    render partial: 'shared/attribute_not_editable', locals: {object: object, attribute: attribute, f: form}
+  def not_editable_for(object, attribute, form, options={})
+    render partial: 'shared/attribute_not_editable', locals: {object: object, attribute: attribute, f: form, options: options}
   end
 end
